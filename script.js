22500adf222b42ba42e357946950ed74267f73b3
2611saurabh <saurabh.kr026@gmail.com>
2023-08-29 11:33:14 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const text = (document.querySelector('.message').textContent);
+
+
+const s = document.querySelector('.message').textContent = 'Correct Number !';
+
+
+ document.querySelector('.number').textContent = 13;
+ document.querySelector('.score').textContent = 10;
+
+
+ document.querySelector('.guess').value = 23;
+ console.log(document.querySelector('.guess').value);
