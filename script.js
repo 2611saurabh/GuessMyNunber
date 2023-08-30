1210150e7278cf06ac869184d262d464042dfb51
2611saurabh <saurabh.kr026@gmail.com>
2023-08-30 11:12:53 +0530

    update

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
 'use strict';
 
-const text = (document.querySelector('.message').textContent);
+// const text = (document.querySelector('.message').textContent);
 
 
-const s = document.querySelector('.message').textContent = 'Correct Number !';
+// const s = document.querySelector('.message').textContent = 'Correct Number !';
 
 
- document.querySelector('.number').textContent = 13;
- document.querySelector('.score').textContent = 10;
+//  document.querySelector('.number').textContent = 13;
+//  document.querySelector('.score').textContent = 10;
 
 
- document.querySelector('.guess').value = 23;
- console.log(document.querySelector('.guess').value);
+//  document.querySelector('.guess').value = 23;
+//  console.log(document.querySelector('.guess').value);
+
+//handling click events
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+
+document.querySelector('.check').addEventListener('click', function()
+{
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess, typeof guess);
+
+    if(!guess){
+
+        document.querySelector('.message').textContent = "No number";
+    }
+    
+    else if(guess === secretNumber)
+    {
+        document.querySelector('.message').textContent = "Correct Number !";
+    }
+    else if(guess > secretNumber)
+    {
+        if(score > 1)
+        {
+            document.querySelector('.message').textContent = "Guess is to high";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = "You lost the game";
+            document.querySelector('.score').textContent = 0;
+        }
+        
+
+    }
+    else if(guess < secretNumber)
+    {
+        if(score > 1)
+        {
+            document.querySelector('.message').textContent = "Guess is to low";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = "You lost the game";
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
